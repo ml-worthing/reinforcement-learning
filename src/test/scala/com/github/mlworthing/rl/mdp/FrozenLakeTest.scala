06ded3d3822cb58fceb546cbb9c2b2b57aa105bf
Pawel Panasewicz <pawel.panasewicz@users.noreply.github.com>
2019-05-14 18:52:41 +0100

    [PP] ML worthing session and implementing frozen lake

--- src/test/scala/com/github/mlworthing/rl/mdp/FrozenLakeTest.scala
+++ src/test/scala/com/github/mlworthing/rl/mdp/FrozenLakeTest.scala
@@ -17,6 +17,7 @@
 package com.github.mlworthing.rl
 package mdp
 
+import com.github.mlworthing.rl.mdp.mdpalgorithm.{MdpDescription, States}
 import com.github.mlworthing.rl.utils.PolicyExecutor
 import org.scalatest.{FreeSpec, Matchers}
 
@@ -28,5 +29,35 @@ class FrozenLakeTest extends FreeSpec with Matchers {
     val policy = agent.solve(FrozenLake)
 
     PolicyExecutor.execute(policy, FrozenLake, maxIterations = 1000, numberOfSamples = 1000)
+  }
+
+  "Solve FrozenLake using MDP" in {
+
+    val board: FrozenLake.Board = FrozenLake.board
+
+
+    type State = Int
+    type Action = String
+
+    val nonTerminalStates: Set[State] = FrozenLake.board.keySet diff FrozenLake.terminalStates.toSet
+
+    val mdpDescription = MdpDescription[State, Action](
+      states = States(
+        terminalStates = FrozenLake.terminalStates,
+        nonTerminalStates = nonTerminalStates
+      ),
+      actions = {
+        case s if nonTerminalStates.contains(s) => FrozenLake.actions
+        case s  => Nil
+      },
+      rewards = {
+        case (s, a) => board(s)(a).map(_._3)
+      },
+      p = {
+        case (ś, r, s, a) => board(s)(a).find(_._1 == ś).map(_._2).getOrElse(0.0)
+      }
+    )
+
+
   }
 }
