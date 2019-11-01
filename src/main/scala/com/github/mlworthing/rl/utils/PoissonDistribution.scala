39584a228fb098f2819c3871f5458762f6b9776f
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-11-01 01:33:16 +0100

    fix Poisson distribution

--- src/main/scala/com/github/mlworthing/rl/utils/PoissonDistribution.scala
+++ src/main/scala/com/github/mlworthing/rl/utils/PoissonDistribution.scala
@@ -11,13 +11,18 @@ case class PoissonDistribution(lambda: Double) {
 
   private val distribution: Cache[Int, Double] = Cache(probability)
 
+  /**
+    * Returns Poisson's probability of value n
+    */
   def apply(n: Int): Double = distribution(n)
 
   /**
-    * Returns Poisson's probability of n with a cap on max value,
+    * Returns Poisson's probability of value n with a cap on max value,
     * where probability of max is 1 minus sum of probabilities from 0 until max.
     */
-  def apply(n: Int, max: Int): Double = if (n < max) distribution(n) else 1d - (0 until max).map(distribution(_)).sum
+  def apply(n: Int, max: Int): Double =
+    if (n < max) distribution(n)
+    else 1d - (0 until max).map(distribution(_)).sum
 
   def probability(n: Int): Double =
     Math.pow(lambda, n) / Factorial(n) * Math.exp(-lambda)
