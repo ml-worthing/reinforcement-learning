2c3af58fa5546264a81b4169fb65272d6d132871
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-11-01 01:31:53 +0100

    fix Poisson distribution

--- src/main/scala/com/github/mlworthing/rl/utils/PoissonDistribution.scala
+++ src/main/scala/com/github/mlworthing/rl/utils/PoissonDistribution.scala
@@ -3,17 +3,13 @@ package com.github.mlworthing.rl.utils
 /**
   * Discrete probability distribution that expresses the probability
   * of a given number of events occurring in a fixed interval of time.
-  *
   * @see https://en.wikipedia.org/wiki/Poisson_distribution
   *
-  * Creates Poisson's distribution given an expected value and a range of possible values.
-  * Probability of minimum value is a sum of probabilities from 0 to min.
-  * Probability of maximum value is a difference between 1 and sum of probabilities from 0 until max.
-  * Probability of a value outside range is 0.
+  * Caches Poisson's distribution given an expected value (lambda).
   */
-case class PoissonDistribution(expected: Int) {
+case class PoissonDistribution(lambda: Double) {
 
-  private val distribution: Cache[Int, Double] = Cache(probability(_, expected))
+  private val distribution: Cache[Int, Double] = Cache(probability)
 
   def apply(n: Int): Double = distribution(n)
 
@@ -23,6 +19,6 @@ case class PoissonDistribution(expected: Int) {
     */
   def apply(n: Int, max: Int): Double = if (n < max) distribution(n) else 1d - (0 until max).map(distribution(_)).sum
 
-  def probability(n: Int, lambda: Int): Double =
+  def probability(n: Int): Double =
     Math.pow(lambda, n) / Factorial(n) * Math.exp(-lambda)
 }
